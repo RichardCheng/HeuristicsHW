ce995de1d642ae8518c00515e6d99597f808d2c9
U-RainG-PC\RainG <rain.gc@gmail.com>
2011-09-21 21:33:18 -0400

    Implementation of GA, crossover, and mutation

---
+++ hw3/mutation.m
@@ -0,0 +1,20 @@
+function children = mutation(currChildren, pMutation)
+
+% For each bit
+for i = 1:size(currChildren,1)
+    for j = 1:size(currChildren,2)
+
+        % If we satisfy the probability
+        if (rand()<pMutation)
+
+            % Flip the bit.
+            if (currChildren(i,j) == '0')
+                currChildren(i,j) = '1'
+            else
+                currChildren(i,j) = '0'
+            end
+        end
+    end
+end
+
+children = currChildren         
