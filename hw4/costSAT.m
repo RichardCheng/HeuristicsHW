c174b674d99abac8384f081e89b031cbcc7ac2d2
Richard <Rain.GC@Gmail.com>
2011-09-29 01:41:50 -0400

    even moar

--- hw4/costSAT.m
+++ hw4/costSAT.m
@@ -4,27 +4,22 @@ global dataleng;
 
 unSAT = 0.;
 for i = 1 : dataleng
-    if ~clauseSAT(solution, i)
-        unSAT = unSAT + 1.;
-    end
-end
-
-percent_unSAT = unSAT / dataleng;
-    
-function bool = clauseSAT(solution, index)
-    global data;
-    bool = 0;
     for i = 1 : 3
+        unSAT = unSAT + 1.;
+
         d = data(index, i);
         if d > 0
             if (solution(d))
-                bool = 1;
+                unSAT = unSAT - 1.;
                 break;
             end
         else
             if (~solution(-d)) 
-                bool = 1;
+                unSAT = unSAT - 1.;
                 break;
             end
         end
     end
+end
+
+percent_unSAT = unSAT / dataleng;
