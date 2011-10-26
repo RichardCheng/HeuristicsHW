9315d500f945577ad1340b563a84423feb9467a8
Hank Duan <duan.hank@gmail.com>
2011-10-26 19:45:05 -0400

    s

--- hw7/GA/main.m
+++ hw7/GA/main.m
@@ -11,14 +11,20 @@ result = zeros(ITERATION, 20);
 
 bestsolution = zeros(200,1);
 
+fprintf('running GA:\n[');
+
 for temp = 1:20
     % Generate initial population. n=?
     InitPop = generatePopulation(POP_SIZE, 20);
 
     result(:, temp) = GAReal2Q(InitPop, POP_SIZE, ITERATION, P_CROSSOVER, P_MUTATION, V);
     
-    bestsolution = (bestsolution .* (temp - 1) + result(:, temp)) ./ temp; 
+    bestsolution = (bestsolution .* (temp - 1) + result(:, temp)) ./ temp;
+    
+    fprintf('%d ', result(ITERATION, temp));
 
 end
+
+fprintf(']\n'); 
 
 plot(50:50:10000, bestsolution, 'r'); 
--- hw7/SA/main.m
+++ hw7/SA/main.m
@@ -4,9 +4,17 @@ alpha = 0.999657;
 repetition = 20; 
 r = 10.*rand(repetition,20);
 bestsolution = zeros(10000,1); 
+
+fprintf('running SA:\n[');
+
 for i = 1: repetition
     [solution, ~] = SA(r(i,:), Tinitial, alpha, 1, 1, 10000);
     bestsolution = (bestsolution .* (i-1) + solution(:, 3)) ./ i; 
+    
+    fprintf('%d ', solution(10000, 3));
+    
 end
+
+fprintf(']\n'); 
     
 plot(1:10000, -bestsolution, 'g:'); 
