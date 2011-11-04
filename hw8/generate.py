2170a3ee6e96a549058621f9a4b48649a4d6c10e
U-RainG-PC\RainG <rain.gc@gmail.com>
2011-11-03 23:24:28 -0400

    Finishing writeup

--- hw8/generate.py
+++ hw8/generate.py
@@ -13,13 +13,13 @@ result = range(0,7)
 
 def main():
     s = [0, 15, 20, 10, 13, 14, 11, 16]
-    T = 2
+    T = 100
 
     for x in range(0, 7):
         result[x] = range(0, 7)
     for t in range(0,7):
         for t2 in range(0,7):
-            result[t][t2] = "0"
+            result[t][t2] = 0 
 
     for t in range(1, 8):
         count = 0
@@ -36,17 +36,24 @@ def main():
                         ss = ("(-%d/%d)" % (100*(s[nxt_ind] - s[t])/gg, T/gg))
                     else:
                         ss = ("-%d" % (100*(s[nxt_ind]-s[t])/gg))
-                    result[t-1][nxt_ind-1] = ("0.25*e^%s" % ss)
+                    result[t-1][nxt_ind-1] = 0.25*math.exp(-100.0*(s[nxt_ind] - s[t])/T)
 
                     sys.stdout.write('S%d -> S%d [ label = "0.25*e^%s" ];\n' % (t, nxt_ind, ss))
                 else:
-                    result[t-1][nxt_ind-1] = "0.25" 
+                    result[t-1][nxt_ind-1] = 0.25
                     sys.stdout.write('S%d -> S%d [ label = "0.25" ];\n' % (t, nxt_ind))
+
+    for t in range(0,7):
+        res = 0
+        for t2 in range(0,7):
+            if (t2 != t):
+                res += result[t][t2]
+        result[t][t] = 1.0 - res
 
     sys.stdout.write("MATRIX: \n")
     for t in range(0,7):
         for t2 in range(0,7):
-            sys.stdout.write("%s " % result[t][t2])
+            sys.stdout.write("%f " % result[t][t2])
         sys.stdout.write("\n")
 
 main()
